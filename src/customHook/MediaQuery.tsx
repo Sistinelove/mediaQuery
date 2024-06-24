822f7241ab45faf99bf5a9aef9f93a59469bcb28
Sistinelove <142403330+Sistinelove@users.noreply.github.com>
2024-06-24 17:35:27 +0500

    last change

--- src/customHook/MediaQuery.tsx
+++ src/customHook/MediaQuery.tsx
@@ -4,7 +4,6 @@ import { useMediaQuery } from "./useMediaQuery";
 import { utilMediaQueryString } from "../utilits/utils";
 
 export const MediaQuery: React.FC<MediaQueryPropsMinOne> = ({children,...props}) => {
-
   const matches = useMediaQuery({
     query: utilMediaQueryString(props)
   });
--- src/utilits/utils.ts
+++ src/utilits/utils.ts
@@ -1,34 +1,29 @@
-import {MediaQueryProps} from "../../types";
+import {type MediaQueryProps} from "../../types";
 
 const parseKey = (str: string): string => {
   return str.replace(/([a-z])([A-Z])/g, '$1-$2').toLowerCase();
 };
-const formatMediaQuery = (key:string, value: any): string => {
+const formatMediaQuery = (key: keyof MediaQueryProps, value: any): string => {
   switch (key) {
     case 'orientation':
-      return `orientation: ${value}`;
+      return `${key}: ${value}`;
     case 'minResolution':
     case 'maxResolution':
       return `${parseKey(key)}: ${typeof value === 'number' ? value + 'dppx' : value}`;
-    case 'minWidth':
-    case 'maxWidth':
+    case "maxWidth":
+    case "minWidth":
     case 'minHeight':
     case 'maxHeight':
       return `${parseKey(key)}: ${value}px`;
     default:
-      return assertNever(key);
+      const n: never = key
+      throw new Error(`Unexpected key: ${n}`);
   }
 };
 
-function assertNever(x: string): never {
-  throw new Error(`Unexpected key: ${x}`);
-}
-
 export const utilMediaQueryString = (props: MediaQueryProps): string => {
   const mediaQueries = Object.entries(props).map(([key, value]) => {
-    if (value !== null) {
-      return formatMediaQuery(key,value);
-    }
+    return formatMediaQuery(key as keyof MediaQueryProps, value);
   });
   return mediaQueries.join(' and ');
 };
